acba03838d1ce35a358d512cbe8fdc8960002d16
Мухин Григорий <muhin.g.s.00@gmail.com>
2025-05-18 16:47:11 +0300

    fix config

--- internal/config/constant_config.go
+++ internal/config/constant_config.go
@@ -3,10 +3,11 @@ package config
 func NewConstantConfig() *Config {
 	return &Config{
 		HTTPServer: HTTPServer{
-			Address: "0.0.0.0:8080",
+			Address: "0.0.0.0:8081",
 		},
 		Storage: Storage{
 			StorageDir: "./storage",
 		},
+		Env: EnvLocal,
 	}
 }
