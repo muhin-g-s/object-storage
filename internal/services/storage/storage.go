24e4a5cb7dc0057553ac74e7167b7191820ff4b8
Мухин Григорий <muhin.g.s.00@gmail.com>
2025-05-18 17:12:57 +0300

    create storage servise

---
+++ internal/services/storage/storage.go
@@ -0,0 +1,51 @@
+package storage
+
+import (
+	"io/ioutil"
+	"log/slog"
+	"object-storage/internal/config"
+	"sync"
+)
+
+type Storage struct {
+	mu     sync.Mutex
+	files  map[string][]byte
+	logger *slog.Logger
+	cfg    *config.Config
+}
+
+func NewStorage() *Storage {
+	return &Storage{
+		files: make(map[string][]byte),
+	}
+}
+
+func (s *Storage) Save(key string, data []byte) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	s.files[key] = data
+
+	err := ioutil.WriteFile(s.cfg.StorageDir+"/"+key, data, 0644)
+	if err != nil {
+		s.logger.Error("Ошибка при сохранении файла %s: %v", key, err)
+	}
+}
+
+func (s *Storage) Load(key string) ([]byte, bool) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	data, exists := s.files[key]
+	if exists {
+		return data, true
+	}
+
+	data, err := ioutil.ReadFile(s.cfg.StorageDir + "/" + key)
+	if err != nil {
+		return nil, false
+	}
+
+	s.files[key] = data
+	return data, true
+}
