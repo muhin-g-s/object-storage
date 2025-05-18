89739285e7733d49c288f318a33aeed23ee63640
Мухин Григорий <muhin.g.s.00@gmail.com>
2025-05-18 16:48:15 +0300

    add graceful shutdown

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,6 +1,53 @@
 package main
 
-func main() {
+import (
+	"context"
+	"net/http"
+	"object-storage/internal/config"
+	"object-storage/internal/infrastructure/api"
+	"object-storage/internal/infrastructure/logger"
+	"object-storage/pkg/logger/sl"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
 
-	panic("not implimented")
+func main() {
+	cfg := config.NewConstantConfig()
+	logger := logger.SetupLogger(cfg.Env)
+
+	logger.Info("starting server", "address", cfg.HTTPServer.Address)
+
+	api := api.NewApi(cfg, logger)
+
+	srv := &http.Server{
+		Addr:    cfg.Address,
+		Handler: api.Router,
+	}
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			logger.Error("cannot start server", sl.Err(err))
+		}
+	}()
+
+	logger.Info("server started")
+
+	<-done
+	logger.Info("stopping server")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		logger.Error("failed to stop server", sl.Err(err))
+
+		return
+	}
+
+	logger.Info("server stopped")
 }
